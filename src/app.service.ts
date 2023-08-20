92b076f683580837423ee06bad1e9e31ac3a7e01
pooreumu <pooreum83@gmail.com>
2023-08-20 17:11:34 +0900

    ✨ 월요일에 5,000원 구매하는 스케줄 추가

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,6 +5,7 @@ import { Cron } from '@nestjs/schedule';
 import { GetWinResultLotteryUseCase } from './use-case/get-win-result-lottery.use-case';
 import { Catch } from './libs/decorator/catch';
 import { GameCount } from './game-count';
+import * as process from 'process';
 
 @Injectable()
 export class AppService {
@@ -16,17 +17,6 @@ export class AppService {
     ) {}
 
     @Catch(AppService.name)
-    @Cron('0 0 18 * * 5')
-    async purchaseLottery() {
-        this.logger.log('purchaseLottery');
-        await this.purchaseLotteryUseCase.execute(GameCount.ONE);
-        await this.slackUseCase.sendNotification(
-            this.purchaseLotteryUseCase.file,
-            'Here you are :four_leaf_clover:',
-        );
-    }
-
-    @Catch(AppService.name)
     @Cron('0 0 22 * * 6')
     async getWinResultLottery() {
         this.logger.log('getWinResultLottery');
@@ -34,6 +24,30 @@ export class AppService {
         await this.slackUseCase.sendNotification(
             this.getWinResultLotteryUseCase.file,
             'WINNER WINNER CHICKEN DINNER :trophy:',
+        );
+    }
+
+    @Catch(AppService.name)
+    @Cron('0 0 18 * * 5')
+    async purchaseLotteryOneGame() {
+        await this.purchaseLottery(GameCount.ONE, process.env.FRIDAY_MESSAGE);
+    }
+
+    @Catch(AppService.name)
+    @Cron('0 0 18 * * 1')
+    async purchaseLotteryFiveGame() {
+        await this.purchaseLottery(GameCount.FIVE, process.env.MONDAY_MESSAGE);
+    }
+
+    private async purchaseLottery(
+        gameCount: GameCount,
+        message = 'Here you are :four_leaf_clover:',
+    ) {
+        this.logger.log('purchaseLottery');
+        await this.purchaseLotteryUseCase.execute(gameCount);
+        await this.slackUseCase.sendNotification(
+            this.purchaseLotteryUseCase.file,
+            message,
         );
     }
 }
