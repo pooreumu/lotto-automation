3d12baed9a08fa17bfcb6e16f745c430a86c76fa
pooreumu <pooreum83@gmail.com>
2024-01-21 19:01:04 +0900

    ✨ 등위 enum 추가

---
+++ src/domain/rank.spec.ts
@@ -0,0 +1,57 @@
+import { ERank } from './rank';
+
+describe('Rank', () => {
+    describe('getRank', () => {
+        it('6개 번호 일치 하면 1등이다', () => {
+            const result = ERank.getRank(6, 0);
+
+            expect(result).toBe(ERank.FISRT_PLACE);
+        });
+
+        it('5개 번호 일치 + 보너스 번호 일치 하면 2등이다', () => {
+            const result = ERank.getRank(5, 1);
+
+            expect(result).toBe(ERank.SECOND_PLACE);
+        });
+
+        it('5개 번호 일치 + 보너스 번호 불일치 하면 3등이다', () => {
+            const result = ERank.getRank(5, 0);
+
+            expect(result).toBe(ERank.THIRD_PLACE);
+        });
+
+        it.each([
+            [4, 0],
+            [4, 1],
+        ])('4개 번호 일치 하면 4등이다', (general: number, bonus: number) => {
+            const result = ERank.getRank(general, bonus);
+
+            expect(result).toBe(ERank.FOURTH_PLACE);
+        });
+
+        it.each([
+            [3, 0],
+            [3, 1],
+        ])('3개 번호 일치 하면 5등이다', (general: number, bonus: number) => {
+            const result = ERank.getRank(general, bonus);
+
+            expect(result).toBe(ERank.FIFTH_PLACE);
+        });
+
+        it.each([
+            [2, 0],
+            [2, 1],
+            [1, 0],
+            [1, 1],
+            [0, 0],
+            [0, 1],
+        ])(
+            '2개 이하로 번호 일치 하면 낙첨이다',
+            (general: number, bonus: number) => {
+                const result = ERank.getRank(general, bonus);
+
+                expect(result).toBe(ERank.NO_PLACE);
+            },
+        );
+    });
+});
---
+++ src/domain/rank.ts
@@ -0,0 +1,111 @@
+import { Enum, EnumType } from 'ts-jenum';
+import { BadRequestException } from '@nestjs/common';
+
+@Enum('code')
+export class ERank extends EnumType<ERank>() {
+    static readonly FISRT_PLACE = new ERank('FISRT_PLACE', '1등', '20억원', [
+        {
+            general: 6,
+            bonus: 0,
+        },
+    ]);
+    static readonly SECOND_PLACE = new ERank(
+        'SECOND_PLACE',
+        '2등',
+        '5,000만원',
+        [
+            {
+                general: 5,
+                bonus: 1,
+            },
+        ],
+    );
+    static readonly THIRD_PLACE = new ERank('THIRD_PLACE', '3등', '150만원', [
+        {
+            general: 5,
+            bonus: 0,
+        },
+    ]);
+    static readonly FOURTH_PLACE = new ERank(
+        'FOURTH_PLACE',
+        '4등',
+        '50,000원',
+        [
+            {
+                general: 4,
+                bonus: 0,
+            },
+            {
+                general: 4,
+                bonus: 1,
+            },
+        ],
+    );
+    static readonly FIFTH_PLACE = new ERank('FIFTH_PLACE', '5등', '5,000원', [
+        {
+            general: 3,
+            bonus: 0,
+        },
+        {
+            general: 3,
+            bonus: 1,
+        },
+    ]);
+    static readonly NO_PLACE = new ERank('NO_PLACE', '낙첨', '0원', [
+        {
+            general: 2,
+            bonus: 0,
+        },
+        {
+            general: 2,
+            bonus: 1,
+        },
+        {
+            general: 1,
+            bonus: 0,
+        },
+        {
+            general: 1,
+            bonus: 1,
+        },
+        {
+            general: 0,
+            bonus: 0,
+        },
+        {
+            general: 0,
+            bonus: 1,
+        },
+    ]);
+
+    private constructor(
+        readonly code: string,
+        readonly name: string,
+        readonly prize: string,
+        readonly matchingNumberCount: {
+            general: number;
+            bonus: number;
+        }[],
+    ) {
+        super();
+    }
+
+    static getRank(
+        matchingGeneralNumberCount: number,
+        matchingBonusNumberCount: number,
+    ): ERank {
+        const rank = this.values().find((rank) =>
+            rank.matchingNumberCount.some(
+                (count) =>
+                    count.general === matchingGeneralNumberCount &&
+                    count.bonus === matchingBonusNumberCount,
+            ),
+        );
+
+        if (!rank) {
+            throw new BadRequestException('당첨 등수를 찾을 수 없습니다.');
+        }
+
+        return rank;
+    }
+}
