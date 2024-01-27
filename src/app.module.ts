2084d8ae17e0ead3dde1ede1b6e3ce9739e24d21
pooreumu <pooreum83@gmail.com>
2024-01-27 18:11:53 +0900

    🔧 typeorm 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,22 @@ import { AppService } from './app.service';
 import { GetWinResultLotteryPuppeteerUseCase } from './use-case/get-win-result-lottery/get-win-result-lottery.puppeteer-use-case';
 import { GET_WIN_RESULT_LOTTERY_USE_CASE } from './use-case/get-win-result-lottery/get-win-result-lottery.use-case';
 import { PURCHASE_LOTTERY_USE_CASE } from './use-case/purchase-lottery/purchase-lottery.use-case';
+import { TypeOrmConfigModule } from './common/config/type-orm/type-orm-config.module';
+import * as process from 'process';
+import path from 'path';
 
 @Module({
-    imports: [ScheduleModule.forRoot(), ConfigModule.forRoot()],
+    imports: [
+        ScheduleModule.forRoot(),
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath:
+                process.env.NODE_ENV === 'test'
+                    ? path.join(__dirname, '../.env.test')
+                    : path.join(__dirname, '../.env'),
+        }),
+        TypeOrmConfigModule,
+    ],
     providers: [
         SlackUseCase,
         AppService,
---
+++ src/common/config/type-orm/type-orm-config.service.ts
@@ -0,0 +1,38 @@
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import path from 'path';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import { ConfigService } from '@nestjs/config';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class TypeOrmConfigService implements TypeOrmOptionsFactory {
+    constructor(private readonly configService: ConfigService) {}
+
+    createTypeOrmOptions():
+        | TypeOrmModuleOptions
+        | Promise<TypeOrmModuleOptions> {
+        return this.configService.get('NODE_ENV') === 'test'
+            ? {
+                  type: 'postgres',
+                  host: this.configService.get('DB_HOST'),
+                  port: +this.configService.get('DB_PORT'),
+                  username: this.configService.get('DB_USERNAME'),
+                  password: this.configService.get('DB_PASSWORD'),
+                  database: this.configService.get('DB_DATABASE'),
+                  entities: [path.join(__dirname, `**/*.entity{.ts,.js}`)],
+                  synchronize: true,
+                  namingStrategy: new SnakeNamingStrategy(),
+              }
+            : {
+                  type: 'postgres',
+                  host: this.configService.get('DB_HOST'),
+                  port: +this.configService.get('DB_PORT'),
+                  username: this.configService.get('DB_USERNAME'),
+                  password: this.configService.get('DB_PASSWORD'),
+                  database: this.configService.get('DB_DATABASE'),
+                  entities: [path.join(__dirname, `**/*.entity{.ts,.js}`)],
+                  synchronize: false,
+                  namingStrategy: new SnakeNamingStrategy(),
+              };
+    }
+}
