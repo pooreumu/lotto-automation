fb89e32b7a404dd7a65a83d834e1ad6f1dd2837f
pooreumu <pooreum83@gmail.com>
2024-01-28 16:31:10 +0900

    ✅ findBy 메소드 테스트

--- src/repository/lottery.type-orm-repository.spec.ts
+++ src/repository/lottery.type-orm-repository.spec.ts
@@ -13,14 +13,11 @@ describe('LotteryTypeOrmRepository', () => {
         module = await Test.createTestingModule({
             imports: [AppModule],
         }).compile();
+
         lotteryRepository = module.get<LotteryRepository>(LOTTERY_REPOSITORY);
         dataSource = module.get<DataSource>(DataSource);
 
         await dataSource.synchronize(true);
-    });
-
-    afterEach(async () => {
-        await module.close();
     });
 
     describe('save', () => {
@@ -51,6 +48,40 @@ describe('LotteryTypeOrmRepository', () => {
             expect(result.id).toBeDefined();
             expect(result.winningNumbers).toEqual([1, 2, 3, 4, 5, 6]);
             expect(result.round).toEqual(1104);
+        });
+    });
+
+    describe('findBy', () => {
+        it('특정 회차에 구매한 모든 Lottery를 반환할 수 있어야 합니다.', async () => {
+            const lotteries = [
+                Lottery.of({
+                    winningNumbers: [1, 2, 3, 4, 5, 6],
+                    round: 1104,
+                }),
+                Lottery.of({
+                    winningNumbers: [11, 12, 13, 14, 15, 16],
+                    round: 1104,
+                }),
+                Lottery.of({
+                    winningNumbers: [21, 22, 23, 24, 25, 26],
+                    round: 1103,
+                }),
+                Lottery.of({
+                    winningNumbers: [31, 32, 33, 34, 35, 36],
+                    round: 1105,
+                }),
+            ];
+            await dataSource.getRepository(Lottery).insert(lotteries);
+
+            const result = await lotteryRepository.findBy({
+                round: 1104,
+            });
+
+            expect(result.length).toBe(2);
+            expect(result[0].round).toBe(1104);
+            expect(result[0].winningNumbers).toEqual([1, 2, 3, 4, 5, 6]);
+            expect(result[1].round).toBe(1104);
+            expect(result[1].winningNumbers).toEqual([11, 12, 13, 14, 15, 16]);
         });
     });
 });
