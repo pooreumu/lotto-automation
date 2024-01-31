488910c325e940cface1e7dbe9b465049f42de37
pooreumu <pooreum83@gmail.com>
2024-01-31 22:41:45 +0900

    ✨ 로또 당첨 번호 조회 응답값 구현

--- src/use-case/get-win-result-lottery/get-win-result-lottery.puppeteer-use-case.ts
+++ src/use-case/get-win-result-lottery/get-win-result-lottery.puppeteer-use-case.ts
@@ -4,6 +4,7 @@ import { Injectable } from '@nestjs/common';
 
 import puppeteer, { Browser, Page } from 'puppeteer';
 
+import { GetWinResultLottery } from './get-win-result-lottery';
 import { GetWinResultLotteryUseCase } from './get-win-result-lottery.use-case';
 
 @Injectable()
@@ -14,6 +15,9 @@ export class GetWinResultLotteryPuppeteerUseCase
     private page: Page;
     private readonly filePath: string;
     private fileName: string;
+    private round: string;
+    private ballNumbers: string[];
+    private bonusNumber: string;
 
     constructor() {
         this.filePath = path.join(process.cwd(), 'screenshots/win-result/');
@@ -24,7 +28,18 @@ export class GetWinResultLotteryPuppeteerUseCase
         await this.assignNewPage();
         await this.gotoWinResultPage();
         await this.takeScreenshot();
+        await Promise.all([
+            this.assignBallNumbers(),
+            this.assignBonusNumber(),
+            this.assignRound(),
+        ]);
         await this.closeBrowser();
+
+        return new GetWinResultLottery(
+            this.round,
+            this.ballNumbers,
+            this.bonusNumber,
+        );
     }
 
     public get file() {
@@ -59,5 +74,56 @@ export class GetWinResultLotteryPuppeteerUseCase
         await winResultElement?.screenshot({
             path: this.filePath + this.fileName,
         });
+    }
+
+    private async assignBallNumbers() {
+        this.ballNumbers = await Promise.all(
+            [
+                '#article > div:nth-child(2) > div > div.win_result > div > div.num.win > p > span:nth-child(1)',
+                '#article > div:nth-child(2) > div > div.win_result > div > div.num.win > p > span:nth-child(2)',
+                '#article > div:nth-child(2) > div > div.win_result > div > div.num.win > p > span:nth-child(3)',
+                '#article > div:nth-child(2) > div > div.win_result > div > div.num.win > p > span:nth-child(4)',
+                '#article > div:nth-child(2) > div > div.win_result > div > div.num.win > p > span:nth-child(5)',
+                '#article > div:nth-child(2) > div > div.win_result > div > div.num.win > p > span:nth-child(6)',
+            ].map(async (selector) => {
+                const ballNumber = await this.page.$eval(
+                    selector,
+                    (el) => el.textContent,
+                );
+                if (!ballNumber) {
+                    throw new Error('ballNumber is null');
+                }
+                return ballNumber;
+            }),
+        );
+    }
+
+    private async assignBonusNumber() {
+        this.bonusNumber = await this.page
+            .$eval(
+                '#article > div:nth-child(2) > div > div.win_result > div > div.num.bonus > p > span',
+                (el) => el.textContent,
+            )
+            .then((bonusNumber) => {
+                if (!bonusNumber) {
+                    throw new Error('bonusNumber is null');
+                }
+                return bonusNumber;
+            });
+    }
+
+    private async assignRound() {
+        this.round = await this.page
+            .$eval(
+                '#article > div:nth-child(2) > div > div.win_result > h4 > strong',
+                (el) => el.textContent,
+            )
+            .then((round) => round?.replace(/\D/g, ''))
+            .then((round) => {
+                if (!round) {
+                    throw new Error('round is null');
+                }
+                return round;
+            });
     }
 }
