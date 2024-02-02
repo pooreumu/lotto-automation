bcb300b94f9b8f159c1b278221ece2689554956a
pooreumu <pooreum83@gmail.com>
2024-02-02 20:46:53 +0900

    🎨 보너스 번호 필드 추가 및 퍼블릭 전환

--- src/dto/compare-winning-numbers-with-win-result.dto.ts
+++ src/dto/compare-winning-numbers-with-win-result.dto.ts
@@ -1,7 +1,8 @@
 export class CompareWinningNumbersWithWinResultDto {
     constructor(
-        private readonly round: number,
-        private readonly sameNumbersCount: number,
-        private readonly createdAt: Date,
+        readonly round: number,
+        readonly matchingGeneralNumberCount: number,
+        readonly matchingBonusNumberCount: number,
+        readonly createdAt: Date,
     ) {}
 }
